d2643c12caccdc8cc1e8d77beb52a518d318f20c
Dwayne Sutherland <dwayneasutherland1@gmail.com>
2022-08-20 21:19:07 +0100

    added functionality to remove templates

--- cmd/template/remove.go
+++ cmd/template/remove.go
@@ -1,6 +1,7 @@
 package template
 
 import (
+	"github.com/Delta-a-Sierra/ReadMe/data"
 	"github.com/spf13/cobra"
 )
 
@@ -8,6 +9,15 @@ var removeCmd = &cobra.Command{
 	Use:   "remove",
 	Short: "Remove a given template",
 	Run: func(cmd *cobra.Command, args []string) {
+		data.LoadData()
+		for index, template := range data.Data.TemplatesInfo {
+			if template.Name == args[0] {
+				slice1 := data.Data.TemplatesInfo[:index]
+				slice2 := data.Data.TemplatesInfo[index+1:]
+				data.Data.TemplatesInfo = append(slice1, slice2...)
+			}
+		}
+		data.WriteData()
 	},
 }
 
