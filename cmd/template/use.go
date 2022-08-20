978d594990efe82b0c27428a7e0db23d10f4281a
Dwayne Sutherland <dwayneasutherland1@gmail.com>
2022-08-20 21:37:19 +0100

    added the functionality to use a template

--- cmd/template/use.go
+++ cmd/template/use.go
@@ -1,16 +1,37 @@
 package template
 
 import (
-	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/Delta-a-Sierra/ReadMe/data"
 	"github.com/spf13/cobra"
 )
 
 var useCmd = &cobra.Command{
 	Use:   "use",
 	Short: "Use a given template",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("use called")
+		data.LoadData()
+
+		var path string
+
+		for _, template := range data.Data.TemplatesInfo {
+			if template.Name == args[0] {
+				path = template.Filepath
+				templateBytes, _ := os.ReadFile(path)
+				ex, err := os.Executable()
+				exPath := filepath.Dir(ex) + "/" + template.Name + ".md"
+
+				err = os.WriteFile(exPath, templateBytes, os.ModePerm)
+				if err != nil {
+					panic(err)
+				}
+				return
+			}
+		}
+
 	},
 }
 
