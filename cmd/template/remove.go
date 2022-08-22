0129fd1cd7afbaad65de26fd443f4c45caa6cd91
Dwayne Sutherland <dwayneasutherland1@gmail.com>
2022-08-22 15:25:32 +0100

    fixed error in template remove

--- cmd/template/remove.go
+++ cmd/template/remove.go
@@ -1,6 +1,8 @@
 package template
 
 import (
+	"fmt"
+
 	"github.com/Delta-a-Sierra/ReadMe/data"
 	"github.com/spf13/cobra"
 )
@@ -12,16 +14,21 @@ var removeCmd = &cobra.Command{
 		if err := data.LoadData(); err != nil {
 			return err
 		}
+		found := false
 		for index, template := range data.Data.TemplatesInfo {
 			if template.Name == args[0] {
 				slice1 := data.Data.TemplatesInfo[:index]
 				slice2 := data.Data.TemplatesInfo[index+1:]
 				data.Data.TemplatesInfo = append(slice1, slice2...)
+				found = true
 			}
 		}
-		if err := data.WriteData(); err != nil {
-			return err
+		if found {
+			if err := data.WriteData(); err != nil {
+				return err
+			}
+			return nil
 		}
-		return nil
+		return fmt.Errorf("unable to find template: '%s'", args[0])
 	},
 }
